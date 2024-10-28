f9511d8e9d15b1ebd9db6dc14957bf5d4dfcdb7d
Gabriel Maia <135866170+gabrielmaia98@users.noreply.github.com>
2024-10-27 22:46:06 -0300

    R code

---
+++ Relatório RH/R/datacleaning.R
@@ -0,0 +1,130 @@
+# carregando pacotes
+library(tidyverse)
+library(openxlsx)
+# library(xtable) - para fazer tabelas no LaTeX
+
+# carregando os dados de um arquivo do excel
+funcionarios <- readxl::read_excel("CATEGORIAS.xlsx", sheet = 2)
+salariosp <- readxl::read_excel("CATEGORIAS.xlsx", sheet = 3)
+
+# configurando cabeçalho
+salariosp[1,] -> cabec
+salariosp[-1,] -> salariosp
+colnames(salariosp) <- cabec
+rm(cabec)
+salariosp <- salariosp[,1:2]
+
+# selecionando apenas colunas preenchidas e ajeitando cabeçalho
+funcionarios[,c(1, 2, 5, 6, 7, 8, 10)] -> funcionarios
+cabec <- funcionarios[1,]
+funcionarios <- funcionarios[-1,]
+colnames(funcionarios) <- cabec
+colnames(funcionarios)[6] <- "Categoria"
+rm(cabec)
+
+# ajeitando formatação e realizando união das duas tabelas
+salariosp$Categoria <- str_to_upper(salariosp$Categoria)
+
+funcionarios <- funcionarios |>
+  left_join(salariosp, by = "Categoria")
+
+colnames(funcionarios)[6] <- "CATEGORIA"
+colnames(funcionarios)[8] <- "NOVO SALÁRIO"
+funcionarios$SALÁRIO <- as.numeric(funcionarios$SALÁRIO)
+funcionarios$`NOVO SALÁRIO` <- as.numeric(funcionarios$`NOVO SALÁRIO`)
+
+# definindo setor e removendo linhas e colunas inúteis
+funcionarios$DEPARTAMENTO <- NA
+funcionarios$DEPARTAMENTO[2:15] <- c("Administrativo")
+funcionarios$DEPARTAMENTO[18:94] <- "Produção"
+funcionarios <- funcionarios[-c(1, 16),]
+funcionarios <- funcionarios[,-c(1,2)]
+
+# setor produção
+producao <- funcionarios[funcionarios$DEPARTAMENTO == "Produção",]
+
+rm(funcionarios)
+
+# filtrar os que terão um novo salário
+producao <- filter(producao, !is.na(producao$`NOVO SALÁRIO`))
+
+# reajuste
+producao$REAJUSTE <- producao$`NOVO SALÁRIO` - producao$SALÁRIO
+
+# custo total
+total = sum(producao$`NOVO SALÁRIO`) - sum(producao$SALÁRIO)
+
+# selecionando e ordernando colunas
+producao <- producao |>
+   select(NOME, SETOR, CATEGORIA, SALÁRIO, `NOVO SALÁRIO`, REAJUSTE) |>
+   separate(CATEGORIA, into = c("FUNÇÃO", "NÍVEL"), sep = " ", remove = FALSE)
+producao[producao$FUNÇÃO == "ENCARREGADO",]$NÍVEL <- "ENC"
+
+# reajuste total por categoria
+
+reajuste <- producao |>
+  group_by(CATEGORIA) |>
+  left_join(salariosp, by = c("CATEGORIA" = "Categoria")) |>
+  summarise(QTD = n(),
+            TOTAL = sum(REAJUSTE), 
+            PROPOSTA = as.numeric(Salário), 
+            MED_OLD = mean(SALÁRIO),
+            ) |>
+  distinct() |>
+  separate(CATEGORIA, into = c("FUNÇÃO", "NÍVEL"), sep = " ", remove = FALSE)
+reajuste[reajuste$FUNÇÃO == "ENCARREGADO",]$NÍVEL <- "ENC"
+
+rm(salariosp)
+
+# formatando para output
+
+producao$SETOR <- producao$SETOR |> 
+  str_extract("^[^ ]+") |>
+  str_replace_all("REFORMAS", "REFORMA")
+
+setorizado <- producao |> 
+  group_by(SETOR) |>
+  summarise(QTD = n(),
+            Antigo_Salario = sum(SALÁRIO),
+            Reajuste = sum(REAJUSTE),
+            Novo_Salario = sum(`NOVO SALÁRIO`))
+
+setorizado <- setorizado |> 
+  rename(Setor = SETOR)
+
+setorizado$Setor <- str_to_title(setorizado$Setor)
+
+
+# salvando
+
+save(producao, file = "producao.RData")
+save(reajuste, file = "reajuste.RData")
+save(setorizado, file = "setorizado.RData")
+
+# anonimizando os dados
+## producao$NOME <- paste("Funcionário", 1:52)
+
+# salvando no formato excel
+## openxlsx::write.xlsx(producao, file = "reajuste.xlsx")
+
+# formatando para LaTeX
+
+# reajuste <- reajuste |>
+#   mutate(TOTAL = paste0("R$ ", format(TOTAL, nsmall = 2)),
+#          PROPOSTA = paste0("R$ ", format(PROPOSTA, nsmall = 2)),
+#          MED_OLD = paste0("R$ ", format(MED_OLD, nsmall = 2)),
+#          )
+
+# producao
+
+# producao$NOME <- str_extract(producao$NOME, "^[^ ]+ [^ ]+")
+## producao$NOME <- str_to_title(producao$NOME)
+# producao <- producao |>
+#   mutate(SALÁRIO = paste0("R$ ", format(SALÁRIO, nsmall = 2)),
+#          `NOVO SALÁRIO` = paste0("R$ ", format(`NOVO SALÁRIO`, nsmall = 2)),
+#          REAJUSTE = paste0("R$ ", format(REAJUSTE, nsmall = 2))
+#            )
+## print(xtable(producao[,-c(2, 3, 4, 5)], row.names = FALSE), 
+##        include.rownames = FALSE, type = "latex")
+## print(xtable(setorizado, row.names = FALSE), 
+##      include.rownames = FALSE, type = "latex")
---
+++ Relatório RH/R/plotting.R
@@ -0,0 +1,221 @@
+# pacotes utilizados:
+
+library(tidyverse)
+library(fmsb)
+library(scales)
+library(showtext)
+showtext_auto()
+font_add_google("IBM Plex Sans", "plex-sans") # fonte do relatório para padronização. Obrigado ChatGPT por me falar dessa função.
+
+# carregar dados:
+
+load("producao.RData")
+load("reajuste.RData")
+load("setorizado.RData")
+
+# reajuste por categoria
+
+ggplot(data = reajuste, aes(
+  x = TOTAL,
+  y = reorder(CATEGORIA, TOTAL)
+)) +
+  labs(title = "Investimento Mensal por Categoria",
+       subtitle = "Total de recursos adicionais destinados a cada categoria",
+       x = NULL,
+       y = NULL) +
+  geom_bar(stat = "identity", fill= "#6eaff1") +
+  geom_vline(xintercept = 0, colour = "#ff7f0e") +
+  scale_x_continuous(labels = 
+                       label_dollar(prefix = "R$ ", scale = 1e-3,,
+                                    big.mark = ".",
+                                    suffix = "k")) +
+  theme_minimal(base_family = "plex-sans") +
+  theme(plot.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 8, vjust = 3),
+        axis.text.y = element_text(angle = 0, size = 6, face = "bold"),
+        axis.text.x = element_text(size = 8)) 
+
+## gasto mensal
+
+gasto <- producao |>
+  summarise(Antigo = sum(SALÁRIO), Reajuste = sum(`NOVO SALÁRIO`)) |>
+  pivot_longer(cols = everything(), 
+               names_to = "Gasto", 
+               values_to = "Valores")
+
+# gasto$Diferença[1] <- 0
+# gasto$Diferença[2] <- gasto$Valores[2] - gasto$Valores[1]
+# gasto <- rbind(gasto, tibble(Gasto = "Diferença", 
+#                           Valores = gasto$Valores[2] - gasto$Valores[1]))
+
+gasto <- cbind(gasto, Diferença = c(0, 14057))
+
+ggplot(data = gasto, aes(x = Gasto, y = Valores, fill = Diferença)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  scale_y_continuous(
+    labels = label_number(big.mark = ".", scale = 1e-3,
+                          prefix = "R$ ", suffix = "k"),
+    expand = expansion(mult = c(0, 0.1))
+    ) +
+  theme_minimal(base_family = "plex-sans") +
+  labs(title = "Evolução dos Salários", 
+       subtitle = "Impacto dos Reajustes Recentes") + 
+  theme(
+    plot.title = element_text(size = 12, face = "bold", hjust = 0.34),
+    plot.subtitle = element_text(size = 10, face = "plain", , hjust = 0.34),
+    axis.text.y = element_text(size = 6, face = "plain"), 
+    axis.title.y = element_blank(),
+    axis.title.x = element_blank(),
+    legend.position = "none")
+
+
+# dumbbell plot - comparativo entre salários antes e depois
+
+ggplot(data = reajuste, aes(x = PROPOSTA, y = CATEGORIA)) +
+  geom_segment(aes(x = PROPOSTA, xend = MED_OLD, 
+                   y = CATEGORIA, yend = CATEGORIA)) +  
+  geom_point(aes(x = PROPOSTA), 
+             size = 3, colour = "#ff7f0e", show.legend = FALSE) +  
+  geom_point(aes(x = MED_OLD), 
+             size = 3, colour = "#1f77b4", show.legend = FALSE) +  
+  scale_x_continuous(expand = c(0.2, 0.3),
+                     labels = label_number(
+                       scale = 1e-3,
+                       big.mark = ".", 
+                       prefix = "R$ ", suffix = "k")
+  ) +
+  theme_minimal(base_family = "plex-sans") +
+  labs(title = "Comparação dos Salários Médios",
+       subtitle = "Impacto do Reajuste nos Salários") +
+  theme(plot.title = element_text(face = "bold", size = 12),
+        plot.subtitle = element_text(face = "plain", vjust = 2, size = 10),
+        axis.text.y = element_text(size = 8, face = "bold"),
+        axis.text.x = element_text(size = 8, face = "bold"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        plot.margin = margin(t = 10, r = 20, b = 7, l = 0))
+
+## stacked bar plot
+
+reajuste$NÍVEL[6] <- "3"
+
+sbp <- reajuste |>
+  ungroup() |>
+  select(c("FUNÇÃO", "NÍVEL", "QTD", "PROPOSTA"))
+
+sbp$TOTAL <- sbp$QTD * sbp$PROPOSTA
+sbp$FUNÇÃO <- str_to_title(sbp$FUNÇÃO)
+sbp <- sbp |> rename(Nível = NÍVEL)
+
+ggplot(sbp, aes(fill = Nível, 
+                     y = TOTAL,
+                     x = reorder(FUNÇÃO, TOTAL))) +
+  geom_bar(position="stack", stat="identity") +
+  theme_minimal(base_family = "plex-sans") +
+  labs(title = "Distribuição Salarial por Função",
+       subtitle = "Gastos por Função na Folha de Pagamento") +
+  scale_y_continuous(labels = label_number(
+    scale = 1e-3,
+    suffix = "k",
+    big.mark = ".", decimal.mark = ",", 
+    prefix = "R$ "),
+  ) +
+  scale_fill_manual(values = c("1" = "#ff7f0e", 
+                               "2" = "#1f77b4", 
+                               "3" = "#192b40")) +
+  theme(plot.title = element_text(face = "bold", size = 12),
+        plot.subtitle = element_text(face = "plain", size = 10),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size = 7))
+
+
+# recursos por setor
+
+## data wrangling
+setorizado_longer <- setorizado |>
+  pivot_longer(cols = c("Antigo_Salario", "Novo_Salario"),
+               names_to = c("Gasto"), values_to = c("Valores"))
+
+setorizado_longer <- setorizado_longer |>
+  select(Setor, Gasto, Valores, Reajuste)
+
+## plot
+
+ggplot(setorizado_longer, aes(x = reorder(Setor, Valores), 
+                       y = Valores, fill = Gasto)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  scale_y_continuous(labels = label_number(
+    scale = 1e-3,
+    suffix = "k",
+    big.mark = ".", decimal.mark = ",", 
+    prefix = "R$ "),
+  ) +
+  scale_fill_manual(values = c("Antigo_Salario" = "#ff7f0e", 
+                               "Novo_Salario" = "#1f77b4"),
+                    labels = c("Antigo_Salario" = "Anterior",
+                               "Novo_Salario" = "Novo")
+  ) +
+  labs(title = "Impacto dos Reajustes Salariais por Setor") +
+  theme_minimal(base_family = "plex-sans") +
+  theme(plot.title = element_text(face = "bold", size = 12),
+        plot.subtitle = element_text(face = "plain", size = 10),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size = 6, angle = 20)) 
+  
+
+# setores mais beneficiados pela mudança
+
+setorizado_wide <- setorizado |>
+  select(Setor, Novo_Salario) |>
+  pivot_longer(cols = Novo_Salario, names_to = "Tipo", values_to = "Valor") |>
+  pivot_wider(names_from = Setor, values_from = Valor)
+
+setorizadow <- setorizado_wide[,-1]
+
+setorizadow <- rbind(rep(max(setorizado$Novo_Salario), 9), 
+                     rep(min(setorizado$Novo_Salario), 9), 
+                     setorizadow)
+
+radarchart(setorizadow, pcol=rgb(0.08,0.14,0.21,0.8) , 
+           pfcol=rgb(0.12,0.46,0.7,0.7) , plwd=4 , 
+           cglcol="grey", cglty=1, axislabcol="grey", 
+           caxislabels=seq(0,20,5), cglwd=0.8,
+           vlcex=0.6, title = "Distribuição de Recursos"
+) 
+
+# perspectiva de carreira
+
+p1 <- ggplot(sbp[-6,], aes(x = Nível, y = PROPOSTA, fill = Nível)) +
+  geom_col() +
+  geom_text(aes(label = label_number(
+    scale = 1, suffix = ",00",
+    big.mark = ",", decimal.mark = ".",
+    prefix = "R$ ")(PROPOSTA)),
+    position = position_stack(vjust = 0.5), 
+    color = "white", size = 2) + 
+  scale_fill_manual(values = c("3" = "#ff7f0e", 
+                               "2" = "#1f77b4", 
+                               "1" = "#192b40")) +
+  theme_light(base_family = "plex-sans") + 
+  theme(plot.title = element_text(face = "bold", size = 12),
+        plot.subtitle = element_text(face = "plain", size = 10),
+        axis.text.x = element_text(size = 10, face = "bold")) +
+  scale_y_continuous(labels = label_number(
+    scale = 1e-3,
+    suffix = "k",
+    big.mark = ",",
+    prefix = "R$ "
+  ))
+
+
+p1 + 
+  facet_wrap(~ FUNÇÃO, nrow = 4) + 
+  labs(title = "Perspectivas de Progressão de Carreira",
+       subtitle = "Possibilidades de aumento de salário no setor de produção",
+       y = "Salários") +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())  
+
